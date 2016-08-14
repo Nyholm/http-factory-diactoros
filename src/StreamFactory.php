b654e0d6bcb0e76f3729bce07d734a0f211599d9
Woody Gilk <woody.gilk@gmail.com>
2016-08-14 15:53:27 -0500

    Implement single method interfaces
    Refs http-interop/http-factory#1

--- src/StreamFactory.php
+++ src/StreamFactory.php
@@ -4,32 +4,11 @@ namespace Http\Factory\Diactoros;
 
 use Psr\Http\Factory\StreamFactoryInterface;
 use Zend\Diactoros\Stream;
-use Zend\Diactoros\CallbackStream;
 
 class StreamFactory implements StreamFactoryInterface
 {
-    public function createStream()
+    public function createStream($resource)
     {
-        $stream = tmpfile();
-
-        return $this->createStreamFromResource($stream);
-    }
-
-    public function createStreamFromCallback(callable $callback)
-    {
-        return new CallbackStream($callback);
-    }
-
-    public function createStreamFromResource($body)
-    {
-        return new Stream($body);
-    }
-
-    public function createStreamFromString($body)
-    {
-        $stream = tmpfile();
-        fwrite($stream, $body);
-
-        return $this->createStreamFromResource($stream);
+        return new Stream($resource);
     }
 }
